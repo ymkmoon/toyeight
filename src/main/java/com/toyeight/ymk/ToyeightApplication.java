6569fb9b8793d03bb9729105b29859537d5bbdaf
DESKTOP-JEM1I4K\geosoft <ymkmoon43@gmail.com>
2021-10-20 12:46:32 +0900

    chore: Add spring boot configuration

---
+++ src/main/java/com/toyeight/ymk/ToyeightApplication.java
@@ -0,0 +1,13 @@
+package com.toyeight.ymk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyeightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyeight/ymk/ToyeightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyeight.ymk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyeightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
